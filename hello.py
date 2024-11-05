b8969de52909837c9c72c18f17c799a93c5f5b09
shardassaut <sharhabia@gmail.com>
2024-11-05 19:40:12 +0100

    Add mp4 download

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 from flask import *
-import os
+import subprocess
 
 app = Flask(__name__)
 
@@ -7,8 +7,10 @@ app = Flask(__name__)
 def hello_world():
     if request.method == "POST":
         link = request.form.get('url')
-        cmd = f"yt-dlp {link}"
-        os.system(cmd)  
+        cmd = f"yt-dlp {link} -q --list-formats |grep mp4 |tail -n 1| cut -c1-3"
+        format = subprocess.check_output(cmd, shell=True, text="Text")
+        cmd = f"yt-dlp {link} -f {format}"
+        subprocess.run(cmd, shell=True)
         return render_template('main.html')
     else:
         return render_template('main.html')
